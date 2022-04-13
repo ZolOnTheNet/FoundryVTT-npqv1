cc6cdf420bd38d34ab2e958d6d3e196cbc75e5a5
Foundry <foundryvtt@smtp.univ-lyon1.fr>
2022-04-13 15:33:49 +0200

    recuperation des fichiers

---
+++ module/helpers/config.mjs
@@ -0,0 +1,25 @@
+export const NPQV1 = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ NPQV1.abilities = {
+  "for": "NPQV1.AbilityFor",
+  "ag": "NPQV1.AbilityAg",
+  "con": "NPQV1.AbilityCon",
+  "ig": "NPQV1.AbilityIg",
+  "it": "NPQV1.AbilityIt",
+  "v": "NPQV1.AbilitV",
+  "p": "NPQV1.AbilityCha"
+};
+
+NPQV1.abilityAbbreviations = {
+  "for": "NPQV1.AbilityForAbbr",
+  "ag": "NPQV1.AbilityAgAbbr",
+  "con": "NPQV1.AbilityConAbbr",
+  "ig": "NPQV1.AbilityIgAbbr",
+  "it": "NPQV1.AbilityItAbbr",
+  "v": "NPQV1.AbilityVAbbr",
+  "p": "NPQV1.AbilityPAbbr"
+};
